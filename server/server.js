e63a647e76b46c958c3c38ebaba39a62351672bd
Akatronix <xazan6501@gmail.com>
2025-12-08 11:30:58 +0100

    modified server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 const SocketRoutes = require("./src/routes/socket.route");
 const authRoutes = require("./src/routes/auth/auth.route");
 const userRoutes = require("./src/routes/auth/user.route");
-const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,14 +14,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+// home route
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
 // routes
 app.use("/api/sockets", SocketRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
-
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
 
 mongoose
   .connect(process.env.MONGO_URI)
