d89a511609b82d16090c72d6b401fe2e9c6ae981
Akatronix <153951875+Akatronix@users.noreply.github.com>
2025-12-17 02:28:42 +0100

    Change numeric fields to string with default values
    Changed voltage, current, power, and energy fields to strings with default values.

--- server/src/models/Socket.model.js
+++ server/src/models/Socket.model.js
@@ -6,10 +6,10 @@ const socketSchema = new mongoose.Schema(
     location: { type: String, required: true },
     type: { type: String, required: true },
 
-    voltage: { type: Number, default: 0 },
-    current: { type: Number, default: 0 },
-    power: { type: Number, default: 0 },
-    energy: { type: Number, default: 0 },
+    voltage: { type: String, default: "0.000000" },
+    current: { type: String, default: "0.000000" },
+    power:   { type: String, default: "0.000000" },
+    energy:  { type: String, default: "0.000000" },
 
     switchStatus: { type: Boolean, default: false },
     userID: { type: String, required: true },
