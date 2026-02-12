8c034181bf3529879a8e2542147822a0ac06eaea
Akatronix <153951875+Akatronix@users.noreply.github.com>
2026-02-12 09:18:49 +0100

    Add maxPower field to Socket schema
    Added a new field 'maxPower' to the socket schema, defaulting to null.

--- server/src/models/Socket.model.js
+++ server/src/models/Socket.model.js
@@ -1,3 +1,27 @@
+// const mongoose = require("mongoose");
+
+// const socketSchema = new mongoose.Schema(
+//   {
+//     name: { type: String, required: true },
+//     location: { type: String, required: true },
+//     type: { type: String, required: true },
+
+//     voltage: { type: String, default: "0.000000" },
+//     current: { type: String, default: "0.000000" },
+//     power:   { type: String, default: "0.000000" },
+//     energy:  { type: String, default: "0.000000" },
+
+//     switchStatus: { type: Boolean, default: false },
+//     userID: { type: String, required: true },
+//   },
+//   { timestamps: true }
+// );
+
+// const Socket = mongoose.model("Socket", socketSchema);
+// module.exports = Socket;
+
+
+
 const mongoose = require("mongoose");
 
 const socketSchema = new mongoose.Schema(
@@ -10,6 +34,9 @@ const socketSchema = new mongoose.Schema(
     current: { type: String, default: "0.000000" },
     power:   { type: String, default: "0.000000" },
     energy:  { type: String, default: "0.000000" },
+
+    // --- NEW FIELD ADDED HERE ---
+    maxPower: { type: String, default: null }, // Default to null to signify "no limit set"
 
     switchStatus: { type: Boolean, default: false },
     userID: { type: String, required: true },
